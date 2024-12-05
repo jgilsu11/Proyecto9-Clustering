2f9a7991d4460009d2ac628589dce6c71df42c6b
jgilsu11 <jaimegilnero@gmail.com>
2024-12-05 17:26:02 +0100

    subida debugueo teclado

--- src/soporte_preprocesamiento_clustering.py
+++ src/soporte_preprocesamiento_clustering.py
@@ -569,4 +569,4 @@ class Clustering:
             "silhouette_score": silhouette,
             "davies_bouldin_index": davies_bouldin,
             "cardinalidad": cardinalidad
-        }, index = [0])
+        })
